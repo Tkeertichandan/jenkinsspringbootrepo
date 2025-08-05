3e273190030a99e65d1b413279368e99bcf2921b
TKC <2300033901@kluniversity.in>
2025-08-05 12:43:33 +0530

    spring boot project v1.0

---
+++ src/main/java/com/klef/ClientController.java
@@ -0,0 +1,22 @@
+package com.klef;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientController 
+{
+	@GetMapping("/")
+  public String home()
+  {
+	  return "CI/CD and Cloud DevOps";
+  }
+	
+    @GetMapping("/demo")
+    public String hdemo()
+	  {
+ 	  return "Jenkins Demo";
+	  }	
+	
+	
+}
---
+++ src/main/java/com/klef/SpringBootDemoProjectApplication.java
@@ -0,0 +1,18 @@
+package com.klef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringBootDemoProjectApplication extends SpringBootServletInitializer
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SpringBootDemoProjectApplication.class, args);
+		System.out.println("Hello DevOps is Running");
+		
+	}
+
+}
